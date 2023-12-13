a18cf8eeeb352a35b840b9f8c18b83d0915e65c4
Dani Guardiola <hi@daniguardio.la>
2023-12-13 23:00:36 +0100

    Replace `window` with `globalThis` (#5)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 export type Browser = typeof import("webextension-polyfill");
 
-// @ts-expect-error Ignoring missing window type.
-export const browser: Browser = window.browser ?? window.chrome;
+// @ts-expect-error Ignoring missing globalThis type.
+export const browser: Browser = globalThis.browser ?? globalThis.chrome;
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 export type Chrome = import("./chrome-types/index.d.ts").Chrome;
 
-// @ts-expect-error Ignoring missing window type.
-export const chrome: Chrome = window.chrome ?? window.browser;
+// @ts-expect-error Ignoring missing globalThis type.
+export const chrome: Chrome = globalThis.chrome ?? globalThis.browser;
